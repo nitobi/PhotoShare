fdb62b121fe969ce85c7886bbba971d3520ea530
Herm Wong <herm.wong@gmail.com>
2011-07-19 10:48:34 -0700

    phonegap shell project

---
+++ src/com/photoshare/PhotoShare.java
@@ -0,0 +1,14 @@
+package com.photoshare;
+
+//import android.app.Activity;
+import android.os.Bundle;
+import com.phonegap.*;
+
+public class PhotoShare extends DroidGap {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
